99b5d6b3427893aaa346f15810d5310ecad6fac6
15236961822 <15236961822@163.com>
2017-02-13 13:55:27 +0800

    fekit

---
+++ src/script/app.js
@@ -0,0 +1,8 @@
+require('./lib/zepto.js');
+require('./lib/angular-1.3.8.min.js');
+require('./lib/angular-ui-router.min.js');
+require('./lib/swiper.js');
+
+require('./usage/commit.js');
+require('./usage/config.js');
+require('./usage/controller.js');
